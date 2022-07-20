ac5d66d06c123ad373177f9c4d9a2345d6fa8fe9
Darshan Kale <darshankale49@gmail.com>
2022-07-20 18:13:01 +0530

    Navbar done today

---
+++ components/navbar.js
@@ -0,0 +1,49 @@
+let navbar = () =>{
+    return `
+
+    <div>
+        <a href="#">HOME</a>
+    </div>
+    <div>
+        <a href="#">OUTLOOK</a>
+    </div>
+    <div>
+        <a href="#">BUSINESS</a>
+    </div>
+    <div>
+        <a href="#">MONEY</a>
+    </div>
+    <div>
+        <a href="#">CRYPTO CORNER</a>
+    </div>
+
+    <div>
+        <a href="#">TRAVEL</a>
+    </div>
+    <div>
+        <a href="#">SPORTS</a>
+    </div>
+    <div>
+        <a href="#">VIDEOS</a>
+    </div>
+    <div>
+        <a href="#">ENTERTAINMENT</a>
+    </div>
+    <div>
+        <a href="#">PHOTOS</a>
+    </div>
+    <div>
+        <a href="#">MAGAZINE</a>
+    </div>
+    <div>
+        <a href="#">HINDI</a>
+    </div>
+    <div>
+        <a href="#">OTHERS</a>
+    </div>
+    <div>
+        <a href="#">AGRITECH</a>
+    </div>`
+}
+
+export default navbar
