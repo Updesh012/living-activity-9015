cb1675e4450e402fc9d766d2d44c39c91fd5a95d
khalidshah12 <kssshah6@gmail.com>
2022-07-23 07:15:07 +0530

    added top news section

--- khalid_home/index.js
+++ khalid_home/index.js
@@ -1,25 +1,76 @@
 
 let key = `0a3107f541194a9e9b886bdcbbb671d0`
-
 let topHeadlines = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${key}`
+let entertainmentUrl = `https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=${key}`
+
 
 let displayData = (data) => {
-    const nationalFeatures = document.querySelector('#nationalFeatures');
+    
     data.forEach((elem) => {
 
-        const nfDiv1 = document.querySelector('#nfDiv1');
         const nfDiv1Img = document.querySelector('#nfDiv1Img');
         const nfDiv1SideHeading = document.querySelector('#nfDiv1SideHeading');
-        const nfDiv1Content = document.querySelector('#nfDiv1Content');
         const nfDiv1PublishedBy = document.querySelector('#nfDiv1PublishedBy');
         const nfDiv1Description = document.querySelector('#nfDiv1Description');
+        const nfDiv1PublishedByDate = document.querySelector('#nfDiv1PublishedByDate');
 
         nfDiv1Img.src = elem.urlToImage;
         nfDiv1SideHeading.innerText = elem.title;
-        nfDiv1Content.innerText = elem.content;
-        nfDiv1PublishedBy.innerText = `${source.name} | ${elem.publishedAt}`;
-        nfDiv1Description.innerText = elem.description;
+        nfDiv1Description.innerText = elem.description + elem.content;
+        nfDiv1PublishedByDate.innerText = ` | ${elem.publishedAt}`
+        nfDiv1PublishedBy.innerHTML = `${elem.source.name}`
+    })
+}
 
+let displayShort = (data) => {
+    let idNumber = 1
+    let count = 0
+    data.forEach((elem) => {
+
+        const img = document.createElement('img');
+        img.classList = "smallImg"
+        img.src = elem.urlToImage
+
+        const title = document.createElement('h3');
+        title.classList = "titleSmall"
+        title.innerText = elem.title
+
+        const description = document.createElement('p');
+        description.classList = "descriptionSmall"
+        description.innerText = elem.description
+
+        const publishedByDivSmall = document.createElement('div');
+        publishedByDivSmall.classList = "publishedByDivSmall"
+
+        const publishedBySmallName = document.createElement('p');
+        publishedBySmallName.classList = "publishedBySmallName"
+        publishedBySmallName.innerText = `${elem.source.name}`;
+
+        const publishedBy = document.createElement('p');
+        publishedBy.classList = "publishedBy"
+        publishedBy.innerHTML = ` | ${elem.publishedAt}`;
+
+        publishedByDivSmall.append(publishedBySmallName, publishedBy)
+
+        if (count < 2) {
+            const div = document.querySelector(`#nfDiv2div${idNumber}`);
+            count++
+            idNumber++
+            div.append(img, title, description, publishedByDivSmall)
+        }
+
+        else if (count < 4) {
+            const div = document.querySelector(`#nfDiv3div${idNumber}`);
+            count++
+            idNumber++
+            div.append(img, title, description, publishedByDivSmall)
+        }
+        else if (count < 6) {
+            const div = document.querySelector(`#nfDiv4div${idNumber}`);
+            count++
+            idNumber++
+            div.append(img, title, description, publishedByDivSmall)
+        }
     })
 }
 
@@ -27,23 +78,9 @@ let displayData = (data) => {
 fetch(topHeadlines).then((res) => {
     return res.json()
 }).then((res) => {
-    console.log(res.articles)
+    // console.log(res.articles)
     displayData(res.articles)
+    displayShort(res.articles)
 }).catch((err) => {
     console.log(err)
 });
-
-
-
-
-// author: "FP Staff"
-// content: "Presidential elections 2022 results: Murmu defeated joint Opposition candidate Yashwant Sinha to become the 15th president of India. She will succeed Ram Nath Kovind whose term ends on 24 July\r\n Pres… [+3901 chars]"
-// description: "Presidential elections 2022 results: Murmu defeated joint Opposition candidate Yashwant Sinha to become the 15th president of India. She will succeed Ram Nath Kovind whose term ends on 24 July"
-// publishedAt: "2022-07-21T14:28:45Z"
-// source:
-// id: null
-// name: "Firstpost"
-// [[Prototype]]: Object
-// title: "Presidential elections 2022 results LIVE: Murmu surges ahead of Sinha after first round of counting - Firstpost"
-// url: "https://www.firstpost.com/india/presidential-poll-2022-results-live-murmu-crosses-majority-mark-set-to-become-indias-first-tribal-woman-president-10935781.html"
-// urlToImage:
