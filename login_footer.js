7f9b170ce7d07107a7da70ac5c2e8ee6f57c12d6
Darshan Kale <darshankale49@gmail.com>
2022-07-23 17:14:21 +0530

    magazine updated

--- login_footer.js
+++ login_footer.js
@@ -3,6 +3,18 @@
 
 // *************************changing icon***********************
 
+import navbar from "../components/navbar.js"
+document.getElementById("navbar").innerHTML = navbar()
+
+let removeTime = (date = new Date()) =>{
+    return new Date(date.toDateString())
+}
+document.getElementById("demo").innerHTML = new Date().toDateString();
+
+
+
+
+
 let x =document.getElementById("password_icon")
 x.addEventListener("click",change_icon)
 let count =0;
